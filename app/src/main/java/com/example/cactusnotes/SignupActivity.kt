a388650d9aeffdbc9be0447ea9828b18d3443bd5
AndroidDev-C <cemdemircem@gmail.com>
2021-03-12 20:14:21 +0300

    Rename MainActivity.kt to SignupActivity.kt

--- app/src/main/java/com/example/cactusnotes/SignupActivity.kt
+++ app/src/main/java/com/example/cactusnotes/SignupActivity.kt
@@ -1,11 +1,11 @@
 package com.example.cactusnotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
